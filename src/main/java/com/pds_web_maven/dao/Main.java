cfb2f5e3ce4ca41010ea61753aa08fd4e239a53a
Jieco Lao <jiecoandres.lao07@gmail.com>
2024-12-02 16:49:24 +0800

    added getValues()

--- src/main/java/com/pds_web_maven/dao/Main.java
+++ src/main/java/com/pds_web_maven/dao/Main.java
@@ -1,9 +1,10 @@
 package com.pds_web_maven.dao;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args){
         ReadPersonal_info ePI = new ReadPersonal_info();
-//        System.out.println(ePI.readLastName());
-        ePI.getallPIDs();
+        System.out.println(ePI.getValues());
     }
 }
--- src/main/java/com/pds_web_maven/dao/Readpersonal_info.java
+++ src/main/java/com/pds_web_maven/dao/Readpersonal_info.java
@@ -1,15 +1,20 @@
 package com.pds_web_maven.dao;
 
 import com.pds_web_maven.entities.personal_info;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.text.DateFormat;
+import java.time.LocalDate;
+import java.time.Period;
 import org.hibernate.SessionFactory;
+import org.hibernate.Session;
 import org.hibernate.cfg.Configuration;
 
 
 public class ReadPersonal_info {
     private SessionFactory factory;
-    private org.hibernate.Session session;
-    
+    private Session session;
     public void createSession(){
         this.factory = new Configuration()
                     .configure("hibernate.cfg.xml")
@@ -37,7 +42,7 @@ public class ReadPersonal_info {
         return result;
     }
     
-    // experiment
+    // test
     public void getallPIDs(){
         createSession();
         try { 
@@ -55,5 +60,38 @@ public class ReadPersonal_info {
         } finally {
             factory.close();
         }
+    }
+    
+    public Map<String, String>[] getValues(){
+        // id, name, gender, age
+        // ArrayList<HashMap<String, String>> productList;
+        // HashMap<String, HashMap<String, String>> allChanges; 
+        Map<String, String>[] respondent = new HashMap[60];
+        createSession();
+        DateFormat dob;
+        try {
+            session.beginTransaction();
+            List<personal_info> dbresult = session.createNamedQuery("personal_info.findAll", personal_info.class).getResultList();
+            int i = 0;
+            LocalDate currDate = LocalDate.now();
+            for (personal_info p : dbresult){
+                respondent[i] = new HashMap<>();
+                respondent[i].put("p_id", String.valueOf(p.getp_id()));
+                respondent[i].put("fullname", p.getl_name() + ", " + p.getf_name() + " " + p.getm_name());
+                respondent[i].put("gender", p.getSex_id() % 2 == 0 ? "Female" : "Male"); // !Others
+                respondent[i].put("age", String.valueOf(Period.between(LocalDate.parse(String.valueOf(p.getDob())), currDate).getYears()));
+                ++i;
+            }
+            
+            // DEBUGGING 
+            // for (int j=0; j<60; ++j){
+            //     System.out.println(respondent[j].get("age"));
+            // }
+
+            session.getTransaction().commit();
+        } finally {
+            factory.close();
+        }
+        return respondent;
     }
 }
