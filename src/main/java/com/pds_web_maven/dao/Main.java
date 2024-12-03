89773bdb523a6c7994f4fbac273bf1c3a388e456
Jieco Lao <jiecoandres.lao07@gmail.com>
2024-12-03 17:01:53 +0800

    Added family_children

--- src/main/java/com/pds_web_maven/dao/Main.java
+++ src/main/java/com/pds_web_maven/dao/Main.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args){
-        // TODO: continue sa family_bg,
+        // TODO: family_children,
         //       HibernateUtil.xml,
         //       YUNG DATABASEE, WALANG COLUMN FOR NAME EXT. RAAAAAH
         ReadPersonal_Info ePI = new ReadPersonal_Info();
@@ -14,9 +14,12 @@ public class Main {
         ReadFamily_background eFG = new ReadFamily_background();
         List<Map<String, String>> def = eFG.getData();
         
+        ReadFamily_children eFC = new ReadFamily_children();
+        List<Map<String, String>> hij = eFC.getData();
+        
         // DEBUGGER
-        for (Map<String, String> map : def) {
-            System.out.println(map.get("spouse_fullname"));
+        for (Map<String, String> map : hij) {
+            System.out.println(map.get("child_dob"));
         }
     }
 }
---
+++ src/main/java/com/pds_web_maven/dao/ReadFamily_children.java
@@ -0,0 +1,67 @@
+package com.pds_web_maven.dao;
+
+import com.pds_web_maven.entities.family_background;
+import com.pds_web_maven.entities.family_children;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class ReadFamily_children {
+    private SessionFactory factory;
+    private Session session;
+    
+    public void createSession(){
+        this.factory = new Configuration()
+                    .configure("hibernate.cfg.xml")
+                    .addAnnotatedClass(family_children.class)
+                    .buildSessionFactory();
+        this.session = factory.getCurrentSession();
+    }
+    
+    public List<Map<String, String>> getData(){
+        List<Map<String, String>> respondent = new ArrayList<>();
+        createSession();
+        try {
+            session.beginTransaction();
+            List<family_children> dbresult = session.createNamedQuery("family_children.findAll", family_children.class).getResultList();
+            for (family_children f : dbresult){ 
+                Map<String, String> data = new HashMap<>();
+                data.put("fam_bg_id", f.convertFam_ch_id(String.valueOf(f.getFamChId())));
+                data.put("p_id", f.convertPid(String.valueOf(f.getP_id())));
+                data.put("child_fullname", f.convertChild_fn(f.getChildFullname()));
+                data.put("child_dob", f.convertChild_dob(String.valueOf(f.getChildDob())));
+                respondent.add(data);
+            }
+            session.getTransaction().commit();
+        } finally {
+            factory.close();
+        }
+        return respondent;
+    }
+    
+    public Map<String, String> getData(int p_id) {
+        createSession();
+        Map<String, String> respondent = new HashMap<>();
+        try {
+            session.beginTransaction();
+            List<family_children> dbresult = session.createNamedQuery("family_children.findByPID", family_children.class)
+                                                  .setParameter("p_id", p_id)
+                                                  .getResultList();
+            if (!dbresult.isEmpty()) {
+                family_children f = dbresult.get(0);
+                respondent.put("fam_bg_id", f.convertFam_ch_id(String.valueOf(f.getFamChId())));
+                respondent.put("p_id", f.convertPid(String.valueOf(f.getP_id())));
+                respondent.put("child_fullname", f.convertChild_fn(f.getChildFullname()));
+                respondent.put("child_dob", f.convertChild_dob(String.valueOf(f.getChildDob())));
+            }
+            session.getTransaction().commit();
+        } finally {
+            factory.close();
+        }
+        return respondent;
+    }
+}
