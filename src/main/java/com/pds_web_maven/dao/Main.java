50c3be68e21f971ac05f387cf59dbe5aeee502f8
Jieco Lao <jiecoandres.lao07@gmail.com>
2024-12-14 23:17:13 +0800

    Modified addData as a personal_info class

--- src/main/java/com/pds_web_maven/dao/Main.java
+++ src/main/java/com/pds_web_maven/dao/Main.java
@@ -1,6 +1,7 @@
 package com.pds_web_maven.dao;
 
 import com.pds_web_maven.entities.family_children;
+import com.pds_web_maven.entities.personal_info;
 import java.util.List;
 import java.util.Map;
 
@@ -30,10 +31,19 @@ public class Main {
 
         family_children clFC = new family_children(54);
 //        eFC.deleteData(clFC);
+
+        personal_info clPI = new personal_info("1", "1", "1", "1", 
+                "Mentos", "Morales", "Peter", "Jr.", "2000-12-12", "Bulacan", 
+                "69", "69", "A+", "123", "123", "123", "123", "123", "123");
+        personal_info newUser = ePI.addData(clPI);
+        
+        System.out.println(newUser.getp_idtoString());
+        
+        
         
         // DEBUGGER
         for (Map<String, String> map : abc) {
-           System.out.println(map.get("fullname"));
+//           System.out.println(map.get("fullname"));
         }
     }
 }
